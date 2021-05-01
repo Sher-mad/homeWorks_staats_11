dee4cf9e4048d3da5fbcd3f50bf2b9e283bbd02f
Sher-mad <80169323+Sher-mad@users.noreply.github.com>
2021-05-01 12:14:11 +0500

    изменил функиции и добавил тип Status

--- pkg.bank/stats/stats_test.go
+++ pkg.bank/stats/stats_test.go
@@ -3,8 +3,8 @@ package stats_test
 import (
 	"fmt"
 
-	"github.com/Sher-mad/homeWorks_bank_11/pkg/bank/types"
-	"github.com/Sher-mad/homeWorks_stats_11/pkg.bank/stats"
+	"github.com/Sher-mad/homeWorks_bank_11/v2/pkg/bank/types"
+	"github.com/Sher-mad/homeWorks_stats_11/v2/pkg.bank/stats"
 )
 
 func ExampleAvg() {
@@ -13,16 +13,19 @@ func ExampleAvg() {
 			ID:       1,
 			Amount:   200,
 			Category: "Караоке",
+			Status:   "Ok",
 		},
 		{
 			ID:       2,
 			Amount:   300,
 			Category: "Караоке",
+			Status:   "Ok",
 		},
 		{
 			ID:       3,
 			Amount:   400,
 			Category: "Караоке",
+			Status:   "Ok",
 		},
 	}
 	fmt.Println(stats.Avg(payments))
@@ -30,40 +33,43 @@ func ExampleAvg() {
 	// 300
 }
 
-func ExampleTotalCategory() {
+func ExampleTotallnCategory() {
 	payments := []types.Payment{
 		{
 			ID:       10,
 			Amount:   120,
 			Category: "Kafe",
+			Status:   "Ok",
 		},
 		{
 			ID:       11,
 			Amount:   130,
 			Category: "SuperMarket",
+			Status:   "Ok",
 		},
 		{
 			ID:       12,
 			Amount:   140,
 			Category: "Kafe",
+			Status:   "Ok",
 		},
 		{
 			ID:       14,
 			Amount:   150,
 			Category: "SuperMarket",
+			Status:   "Ok",
 		},
 
 		{
 			ID:       14,
 			Amount:   5,
 			Category: "SuperMarket",
+			Status:   "Ok",
 		},
 	}
-	sumPayments := stats.TotalCategory(payments, types.Category("SuperMarket"))
+	sumPayments := stats.TotallnCategory(payments, types.Category("SuperMarket"))
 	fmt.Println(sumPayments)
 	//Output:
 	// 285
-	
-
 
 }
